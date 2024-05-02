82e0e33ab49a217149a2eee60ea72e1dd3f378c7
Agustin <agustinborsaumai@gmail.com>
2024-05-02 16:52:23 -0300

    3 doritos mas tarde... access token conseguido. Taba complicado...

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
 async function searchItems(query: string) {
-    const accessToken = 'APP_USR-3895335878647153-050215-8e3cbff7fb824fded20a01a5c41f82cb-829591830';
-    const apiUrl = `https://api.mercadolibre.com/sites/MLA/search?q=${query}&access_token=${accessToken}`;
-    
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.results;
+  try {
+      const accessToken = 'APP_USR-3895335878647153-050215-8e3cbff7fb824fded20a01a5c41f82cb-829591830';
+      const apiUrl = `https://api.mercadolibre.com/sites/MLA/search?q=${query}&access_token=${accessToken}`;
+      
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return data.results;
+  } catch (error) {
+      console.error('Error en searchItems:', error);
+      throw error; // Lanza el error para que sea capturado por la promesa
   }
+}
 
-  searchItems('iphone').then((results) => {
-    console.log(results);
-  });
+searchItems('iphone').then((results) => {
+  console.log(results);
+}).catch((error) => {
+  console.error('Error en búsqueda de artículos:', error);
+});
